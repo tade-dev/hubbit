98fe4f2b956e7018a6c13ccc59d6a5248afe9954
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-05-24 03:44:24 +0100

    Initial Commit

---
+++ Hubbit/HubbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HubbitApp.swift
+//  Hubbit
+//
+//  Created by BSTAR on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HubbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
